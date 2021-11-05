3a0e000c6e8d35d4dceb7ccb3b71d3f1b0e37d62
Estiven Reyes <estivenreyes2013@hotmail.com>
2021-11-05 02:38:28 -0500

    The change filters works, but not the division of them

--- js/store.js
+++ js/store.js
@@ -3,7 +3,7 @@ const products = [
         id: 1,
         name: "Cristal Genesis",
         price: 0.99,
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC1.png",
         isRecomended: true,
         isBestSeller: false,
         description: "This coin is the Genesis Crystal, which can be exchanged 1: 1 for Protogems (60 Genesis Crystal = 60 Protogem).",
@@ -13,7 +13,7 @@ const products = [
         id: 2,
         name: "Cristal Genesis",
         price: 4.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC2.png",
         isRecomended: true,
         isBestSeller: false,
         description: "This coin is the GenesisCrystal, which can be exchanged 1: 1 for Protogems (300 GenesisCrystal = 300 Protogem)",
@@ -23,7 +23,7 @@ const products = [
         id: 3,
         name: "Cristal Genesis",
         price: 14.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC3.png",
         isRecomended: true,
         isBestSeller: false,
         description: "This coin is the GenesisCrystal, which can be exchanged 980: 980 for Protogems (980 GenesisCrystal = 980 Protogem)",
@@ -33,7 +33,7 @@ const products = [
         id: 4,
         name: "Cristal Genesis",
         price: 29.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC4.png",
         isRecomended: true,
         isBestSeller: false,
         description: "This coin is the GenesisCrystal, which can be exchanged 1: 1 for Protogems (300 GenesisCrystal = 300 Protogem)",
@@ -43,17 +43,7 @@ const products = [
         id: 5,
         name: "Cristal Genesis",
         price: 49.99, 
-        image: "",
-        isRecomended: true,
-        isBestSeller: false,
-        description: "This coin is the GenesisCrystal, which can be exchanged 1: 1 for Protogems (300 GenesisCrystal = 300 Protogem)",
-        type: "Cristal",
-    },
-    {
-        id: 6,
-        name: "Cristal Genesis",
-        price: 49.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC5.png",
         isRecomended: true,
         isBestSeller: false,
         description: "This coin is the GenesisCrystal, which can be exchanged 1: 1 for Protogems (300 GenesisCrystal = 300 Protogem)",
@@ -63,7 +53,7 @@ const products = [
         id: 7,
         name: "Cristal Genesis",
         price: 99.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/GC6.png",
         isRecomended: true,
         isBestSeller: false,
         type: "Cristal",
@@ -72,7 +62,7 @@ const products = [
         id: 8,
         name: "Lunar Blessing",
         price: 4.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/lunarPass.png",
         isRecomended: false,
         isBestSeller: true,
         type: "Battle pass",    
@@ -81,7 +71,7 @@ const products = [
         id: 9,
         name: "Battle Pass/Gnosis Hymn - Basic",
         price:  9.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/battlePass.png",
         isRecomended: false,
         isBestSeller: true,
         type: "Battle pass",    
@@ -90,7 +80,7 @@ const products = [
         id: 10,
         name: "Battle Pass/Gnosis Hymn - Advance",
         price: 19.99, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/battlePass.png",
         isRecomended: true,
         isBestSeller: false,
         type: "Battle pass"
@@ -99,7 +89,7 @@ const products = [
         id: 11,
         name: "Sea Breeze Dandelion",
         price: 1600, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/jeanskin.png",
         isRecomended: true,
         isBestSeller: false,
         description: "Jean’s summer wear. Light and cool, but no less elegant for it. A perfect match for a seaside trip.",
@@ -109,7 +99,7 @@ const products = [
         id: 12,
         name: "SummerTime Sparkle",
         price: 1600, 
-        image: "",
+        image: "./recursos/recharge Cristal Genesis/barbaraSkin.png",
         isRecomended: true,
         isBestSeller: false,
         description: "Barbara’s beach attire. The lovely short skirt brims with summer vitality and carries the fresh air of the ocean.",
@@ -125,41 +115,50 @@ const productsSection = document.getElementById("products");
 const product = document.createElement("a");
 product.className = "product";
 
-product.setAtribute("product", './product.html?id=${item.id}');
+const producTamplate = (item) => {
 
-let tagHtml;
-if(item.isRecommended){
+    product.setAttribute("product", `./product.html?id=${item.id}`);
+    
+    const product = document. createElement("a");
+    produc.className = "product";
+
+    product.setAttribute("href", `./product.html?id=${item.id}`);
+
+    let tagHtml;
+    if(item.isRecommended){
     tagHtml = `<span class="product__tag product__tag--recommended">Recomended</span>`;
-} else if (item.isRecommended){
+    } else if (item.isRecommended){
     tagHtml = `<span class="product__tag product__tag--best-seller">Best seller</span>`;
-}else{
+    }else{
     tagHtml = "";
-}
+    }   
 
-const isAdded = cart.some(productCart => productCart.id === item.id);
-let buttonHtml;
+    const isAdded = cart.some(productCart => productCart.id === item.id);
+    let buttonHtml;
 
-if(isAdded){
-    buttonHtml = `<buttom class="product__cart" disabled>Productio añadido</buttom>`;
-} else {
-    buttonHtml = `<button class="product__cart">Add in the shopping car</button>`;
-}
+    if(isAdded){
+    buttonHtml = `<buttom class="product__cart" disabled>Productio añadido
+    </buttom>`;
+    } else {
+    buttonHtml = `<button class="product__cart">Add in the shopping car
+    </button>`;
+    }
 
-product.innerHTML = `
-imag src="${item.image}"alt ="${product__image}">
+    product.innerHTML = `
+    <img src="${item.image}"alt ="${item.name}" class="product__image">
     <div class = "product__description">
     ${tagHtml}
     <h3 class = "product__price">$ ${item.price}</h3>
     <h2 class = "product__name">${item.name}</h2>
     ${buttonHtml}
     </div>
-`;
+    `;
 
-productsSection.appendChild(product);
+    productsSection.appendChild(product);
 
-const productCartButton = product.querySelector(".product__cart");
+    const productCartButton = product.querySelector(".product__cart");
 
-productCartButtom.addEventListener("click", e => {
+    productCartButtom.addEventListener("click", e => {
     e.preventDefault();
     const productAdded ={
         id: item.id,
@@ -171,8 +170,25 @@ productCartButtom.addEventListener("click", e => {
     cart.push(productAdded);
 
     productCartButton.setAttribute("disabled", true);
-});
+    });
 
-products.forEach(product =>{
-    productTemplte(product);
+    products.forEach(product =>{
+        productTemplate(product);
+    });
+};
+
+
+
+const filterByCategory = document.getElementById("categories");
+
+filterByCategory.addEventListener("change", e => {
+    const category = filterByCategory.value;
+
+    productsSection.innerHTML = "";
+    const filteredByCategory = products.filter((product) => product.
+    type === category);
+
+    filteredByCategory.forEach(product =>{
+        productTemplate(product);
+    });
 });
