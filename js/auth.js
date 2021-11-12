6dc3fd52b744a7c0d977a98f9cf860bee7147c2a
Estiven Reyes <estivenreyes2013@hotmail.com>
2021-11-11 21:51:30 -0500

    Register design

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyADo3PwQB6pTfKo6zaxF_A54Y6sbR5CeJo",
+authDomain: "shopping-cart-790a7.firebaseapp.com",
+projectId: "shopping-cart-790a7",
+storageBucket: "shopping-cart-790a7.appspot.com",
+messagingSenderId: "925482071152",
+appId: "1:925482071152:web:2a240fee616627056870b5",
+measurementId: "G-ZEWMS1486Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
